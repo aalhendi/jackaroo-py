664ef693a25dc099b804fd988429c71b734d241e
aalhendi <abdulrazaq.alhendi@gmail.com>
2022-08-20 17:26:03 +0300

    feat: add logger implementation

---
+++ tests/test_deck.py
@@ -0,0 +1,22 @@
+import Game
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+game = Game.Game(num_players=4)
+
+
+def test_deck_length():
+    deck_len = (game.num_players*13)
+    assert len(game.deck.cards) == deck_len
+
+
+def test_hand_lengths():
+    game.deal_cards()
+    player_hand_lengths = [len(p.hand) for p in game.players]
+    game.deal_cards()
+    game.deal_cards()
+    player_hand_lengths2 = [len(p.hand) for p in game.players]
+
+    assert len(set(player_hand_lengths)) == 1 == len(set(player_hand_lengths2))

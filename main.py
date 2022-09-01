003cb558b27b7ec07afff3a6f25453e531e44dd9
aalhendi <abdulrazaq.alhendi@gmail.com>
2022-09-01 11:41:20 +0300

    chore: add name guard and clean requirements for conda

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python3
+
 from Game import Game
 from utils.logger import logger
 from tqdm import tqdm
 
 
-logger.info("RUN START")
-for _ in tqdm(range(1000)):
-    game = Game(num_players=4)
-    winners = game.run(step=False)
-    if winners[0] and winners[2]:
-        winning_team = 1
-    else:
-        winning_team = 2
-    logger.info(
-        f"Rounds Played: {game.rounds_played} - Winners:{winners} - Winning Team: {winning_team}")
-logger.info("RUN END\n")
+def main():
+    logger.info("RUN START")
+    for _ in tqdm(range(1000)):
+        game = Game(num_players=4)
+        winners = game.run(step=False)
+        if winners[0] and winners[2]:
+            winning_team = 1
+        else:
+            winning_team = 2
+        logger.info(
+            f"Rounds Played: {game.rounds_played} - Winners:{winners} - Winning Team: {winning_team}")
+    logger.info("RUN END\n")
+
+if __name__ == "__main__":
+    main()
